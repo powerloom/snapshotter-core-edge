2d92a3de21f91a1c8c2fe79ddec911a69e9900bc
anomit <anomit.ghosh@gmail.com>
2025-10-11 00:13:58 +0530

    Update gRPC channel host in GenericAsyncWorker to use local collector

--- snapshotter/utils/generic_worker.py
+++ snapshotter/utils/generic_worker.py
@@ -632,7 +632,7 @@ class GenericAsyncWorker(multiprocessing.Process):
         Initializes the gRPC channel and stub for communication with the collector.
         """
         self._grpc_channel = Channel(
-            host='host.docker.internal',
+            host='snapshotter-lite-local-collector',
             port=settings.local_collector_port,
             ssl=False,
         )
